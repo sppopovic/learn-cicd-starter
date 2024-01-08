9ed123b1c585b2134e6ef39c2b82de3a18338230
sppopovic <s.p.popovic@gmail.com>
2024-01-08 14:38:05 +0100

    Fixed errors - 2

--- json.go
+++ json.go
@@ -20,7 +20,8 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 
 func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	dat, err := json.Marshal(payload)
+	//dat, err := json.Marshal(payload)
+	_, err := json.Marshal(payload)
 	if err != nil {
 		log.Printf("Error marshalling JSON: %s", err)
 		w.WriteHeader(500)
