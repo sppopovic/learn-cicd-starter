0cc693ae18b391de4ce5604925423378c8305dc2
sppopovic <s.p.popovic@gmail.com>
2024-01-08 13:35:57 +0100

    Added formatting tasks cicd

--- main.go
+++ main.go
@@ -12,11 +12,10 @@ import (
 
 	"github.com/go-chi/chi"
 	"github.com/go-chi/cors"
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/joho/godotenv"
 
 	"github.com/bootdotdev/learn-cicd-starter/internal/database"
-
-	_ "github.com/go-sql-driver/mysql"
 )
 
 type apiConfig struct {
